1f494bd461200ea3029d179e160b5294ddcf10f8
Carson <zxhshaxue@126.com>
2017-12-21 11:15:10 +0800

    add testView

--- TestGit/ViewController.m
+++ TestGit/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIView *testView;
+
 @end
 
 @implementation ViewController
@@ -17,9 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.testView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (UIView *)testView {
+    if (_testView == nil) {
+        _testView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+        _testView.backgroundColor = [UIColor grayColor];
+    }
+    return _testView;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
